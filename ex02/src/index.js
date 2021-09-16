c1c6fc27d5f597279bd44b41215a722ca3fa660d
Nihadk12 <kadic_niho@hotmail.com>
2021-09-16 12:40:41 +0200

    increment

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myRes = 75;
+
+    //Only change code below this line
+    
+    myRes ++;
+
+        return myRes;;
+
+    //Only change code apove this line
+}
+
+console.log(main());
+
+module.exports = main;
