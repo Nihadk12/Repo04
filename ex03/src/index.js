4de097c65b235e581a826f09f9eab7ea6ca25869
Nihadk12 <kadic_niho@hotmail.com>
2021-09-16 12:57:16 +0200

    decrement

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    var myRes = 16;
+    //only change code below this line
+    myRes --;
+    //only change code apove this line
+    return myRes;
+}
+
+console.log(main());
+
+module.exports = main;
