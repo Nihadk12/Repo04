b9ac60364b54eac5120eac2d165056f18fe2169d
Nihadk12 <kadic_niho@hotmail.com>
2021-09-16 15:40:51 +0200

    string

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myFirstName = "Nihad", myLastName="Kadic", myHomeTown="Gracanica";
+   //only change code below this line
+   
+   //only change code apove this line
+   return {
+       myFirstName,
+       myLastName,
+       myHomeTown
+   };
+}
+
+console.log(main());
+
+module.exports = main;
