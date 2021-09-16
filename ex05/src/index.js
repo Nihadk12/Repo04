58fcec7a5aedba9f544d8231559303fde1f89c6f
Nihadk12 <kadic_niho@hotmail.com>
2021-09-16 13:50:28 +0200

    result

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo) {
+     var product;
+    //only change code below this line
+   product = numOne * numTwo;
+    //only change code apove this line
+    return product;
+}
+
+console.log(main(2.0,2.5).toFixed(1));
+
+module.exports = main;
