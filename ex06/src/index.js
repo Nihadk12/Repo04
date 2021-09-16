c659c66668241e05d036373f754aa5aa3147b361
Nihadk12 <kadic_niho@hotmail.com>
2021-09-16 14:57:58 +0200

    ..

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    var myQuotient;
+   //only change code below this line
+  myQuotient = numOne / numTwo;
+   //only change code apove this line
+   return myQuotient;
+}
+
+console.log(main(5.0,2.0));
+console.log(main(16.5,5.5));
+console.log(main(102.0,25.5));
+
+module.exports = main;
