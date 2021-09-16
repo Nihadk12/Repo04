e07431f9f3dabd7dd91ef0937d1d155336cddc9e
Nihadk12 <kadic_niho@hotmail.com>
2021-09-16 13:11:35 +0200

    exponent

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    var myDecimal = 16.25;
+    var decimalE = 254e-5;
+    //only change code below this line
+    
+    //only change code apove this line
+    return decimalE;
+}
+
+console.log(main());
+
+module.exports = main;
